7212e228a9e74f5c4e397634cb6420b5c90724af
Circle <ja95175@gmail.com>
2023-07-03 22:41:44 +0800

    init commit

---
+++ C-Sharp-Tutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿public class MyFirstApp
+{
+    public static void Main()
+    {
+        float vat = 19;
+        float productA = 3.99f;
+        float productB = 11.99f;
+        float productC = 5.75f;
+
+        float summed = productA + productB + productC;
+        float totalTax = (summed / 100) * vat;
+
+        summed += totalTax;
+
+        Console.WriteLine(summed);
+    }
+}
