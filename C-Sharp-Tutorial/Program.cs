a0276f968d2c67cddb39c49f0e59da74398f8041
Circle <ja95175@gmail.com>
2023-07-03 22:58:57 +0800

    Explicity and implicity variable

--- C-Sharp-Tutorial/Program.cs
+++ C-Sharp-Tutorial/Program.cs
@@ -1,18 +1,15 @@
 ﻿public class MyFirstApp
 {
+    //Explicity
+    float price = 3.99f;
+    string name = "Joana";
+    char letter = 'a';
+    int number = 1000000;
+    bool toggle = true;
+
     public static void Main()
     {
-        float vat = 19;
-        float productA = 3.99f;
-        float productB = 11.99f;
-        float productC = 5.75f;
-
-        //
-        float summed = productA + productB + productC;
-        float totalTax = (summed / 100) * vat;
-
-        summed += totalTax;
-
-        Console.WriteLine(summed);
+        //Implicity
+        var username = "Joana";
     }
 }
