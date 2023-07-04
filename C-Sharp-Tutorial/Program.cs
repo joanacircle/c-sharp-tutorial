588385ddc5057dde87c6b6f30c6d0c7cbd93f47c
Circle <ja95175@gmail.com>
2023-07-04 18:03:27 +0800

    user input

--- C-Sharp-Tutorial/Program.cs
+++ C-Sharp-Tutorial/Program.cs
@@ -6,10 +6,26 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Add(1, 2));
+            Console.WriteLine("Result is: " + Caculate());
             Console.Read();
         }
 
+        // User input
+        public static int Caculate()
+        {
+            Console.WriteLine("Please enter the first number");
+            string num1Input = Console.ReadLine();
+            Console.WriteLine("Please enter the second number");
+            string num2Input = Console.ReadLine();
+
+            int num1 = int.Parse(num1Input);
+            int num2 = int.Parse(num2Input);
+
+            int result = num1 + num2;
+            return result;
+        }
+
+        // Method: return value
         public static int Add(int num1, int num2)
         {
             return num1 + num2;
