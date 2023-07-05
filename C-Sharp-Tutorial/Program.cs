f390da4e1d8cdb922fc4bbca0d2235f22a75fd78
Circle <ja95175@gmail.com>
2023-07-05 21:21:11 +0800

    if statement: shortcut

--- C-Sharp-Tutorial/Program.cs
+++ C-Sharp-Tutorial/Program.cs
@@ -6,7 +6,29 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            NestedIfStatement();
+            IfStatementShortcut();
+        }
+
+        // If statement: shortcut
+        public static void IfStatementShortcut()
+        {
+            int temperature = -5;
+            string stateOfMatter;
+
+            //if (temperature < 0)
+            //    stateOfMatter = "solid";
+            //else
+            //    stateOfMatter = "liquid";
+
+            // in short:
+            stateOfMatter = temperature < 0 ? "solid" : "liquid";
+
+            //
+            temperature += 110;
+            stateOfMatter = temperature > 100 ? "gas" : temperature < 0 ? "solid" : "liquid";
+
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+            Console.ReadKey();
         }
 
         // Switch case
