86f9529af3d5dd019ddc701965435320738081ba
Circle <ja95175@gmail.com>
2023-07-05 20:23:24 +0800

    switch case

--- C-Sharp-Tutorial/Program.cs
+++ C-Sharp-Tutorial/Program.cs
@@ -9,6 +9,42 @@ namespace HelloWorld
             NestedIfStatement();
         }
 
+        // Switch case
+        public static void SwitchCase()
+        {
+            int age = 25;
+
+            switch (age)
+            {
+                case 15:
+                    Console.WriteLine("Too young to party in the club!");
+                    break;
+                case 25:
+                    Console.WriteLine("Good to go!");
+                    break;
+                default:
+                    Console.WriteLine("How old are you then?");
+                    break;
+            }
+
+            string username = "Denis";
+
+            switch (username)
+            {
+                case "Denis":
+                    Console.WriteLine("username is Denis");
+                    break;
+                case "root":
+                    Console.WriteLine("username is root");
+                    break;
+                default:
+                    Console.WriteLine("username is unknown");
+                    break;
+            }
+
+            Console.Read();
+        }
+
         // Nested if statement
         public static void NestedIfStatement()
         {
