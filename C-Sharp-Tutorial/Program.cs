59e91ae9111357e55e806110a28bb53d66d7231e
Circle <ja95175@gmail.com>
2023-07-07 22:14:20 +0800

    jagged array

--- C-Sharp-Tutorial/Program.cs
+++ C-Sharp-Tutorial/Program.cs
@@ -13,7 +13,42 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            NestedForLoop();
+            JaggedArray();
+        }
+
+        // Jagged array
+        public static void JaggedArray()
+        {
+            // declare jaggedArray
+            int[][] jaggedArray = new int[3][];
+
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[3];
+            jaggedArray[2] = new int[2];
+
+            jaggedArray[0] = new int[] { 2, 3, 5, 7, 11 };
+            jaggedArray[1] = new int[] { 1, 3, 5 };
+            jaggedArray[2] = new int[] { 13, 21 };
+
+            //alternative way:
+            int[][] jaggedArray2 = new int[][]
+            {
+                new int[] { 2, 3, 5, 7, 11 },
+                new int[] { 1, 3, 5 }
+            };
+
+            Console.WriteLine("The value in middle if the first entry is {0}",
+                jaggedArray2[0][2]);
+
+            for (int i = 0; i < jaggedArray2.Length; i++)
+            {
+                Console.WriteLine("Element {0}", i);
+                for (int j = 0; j < jaggedArray2[i].Length; j++)
+                {
+                    Console.WriteLine(jaggedArray2[i][j]);
+                }
+            }
+            Console.ReadKey();
         }
 
         // Nested for loop
